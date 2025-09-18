576a8b05f919fd4513ea7604f2145bf0831bd2c0
kavyaNamballa <163406073+kavyaNamballa@users.noreply.github.com>
2025-09-18 10:26:21 +0530

    limits service currency exchange service base code

---
+++ currency-exchange-service/src/main/java/com/learnings/microservice/currency_exchange_service/CurrencyExchangeController.java
@@ -0,0 +1,25 @@
+package com.learnings.microservice.currency_exchange_service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CurrencyExchangeController {
+
+    private final Environment environment;
+    private final CurrencyExchangeRepo currencyExchangeRepo;
+
+    @GetMapping("/currency-exchange/from/{usd}/to/{inr}")
+    public CurrencyExchange getExchangeValue(@PathVariable(name = "usd") String from, @PathVariable(name = "inr") String to) {
+        CurrencyExchange currencyExchange = currencyExchangeRepo.findByFromAndTo(from,to);
+//        CurrencyExchange currencyExchange = new CurrencyExchange(1000L,from,to, BigDecimal.valueOf(50));
+        String port  = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
+    }
+
+}
